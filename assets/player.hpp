9b62ec76852e40d5a3566343d1fdb8ed28295c13
chokopan <puakai2010@gmail.com>
2020-07-14 21:55:03 +0900

    graph

--- assets/player.hpp
+++ assets/player.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include "chokolait.hpp"
+#include "camera.hpp"
 
 CE_BEGIN_NAMESPACE
 
@@ -9,9 +10,13 @@ public:
     CE_SERIALIZE SceneObject camAxis;
     CE_SERIALIZE AnimClip idleClip;
     CE_SERIALIZE AnimClip runClip;
+    
+    CE_SERIALIZE float rotSp;
 
     pTransform tr;
     Animator anim;
+    
+    Cam camscr;
     
     float rottar = 180.f;
     
@@ -26,6 +31,8 @@ public:
         anim = object()->children()[0]->GetComponent<Animator>();
         anim->single(true);
         anim->repeat(true);
+        
+        camscr = camAxis->children()[0]->GetComponent<Cam>();
     }
     void OnUpdate() override {
         bool iw = Input::KeyHold(InputKey::W);
@@ -38,13 +45,16 @@ public:
             if (id) angle = iw ? -90.f : 270.f;
             if (iw) angle = Math::Lerp(angle, 0.f, 0.5f);
             else if (Input::KeyHold(InputKey::S)) angle = Math::Lerp(angle, 180.f, 0.5f);
-            rottar = camAxis->transform()->localRotationEuler().y + angle + 180;
-            tr->localRotationEuler(Vec3(0, rottar, 0));
+            rottar = camscr->ry + angle + 180;
             tr->localPosition(tr->localPosition() + tr->forward() * moveSp * Time::delta());
             setclip(runClip);
         }
         else
             setclip(idleClip);
+        
+        static float rot0 = tr->localRotationEuler().y;
+        rot0 = Math::LerpAngle(rot0, rottar, rotSp * Time::delta());
+        tr->localRotationEuler(Vec3(0, rot0, 0));
     }
     void OnPaint() override {
         
